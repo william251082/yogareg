e68dcba926cd4c28dfa18691f15e056263fe3281
William del Rosario <williamdelrosario@yahoo.com>
2018-06-12 09:37:31 +0200

    Controllers done

--- app/Http/Controllers/Company/CompanyController.php
+++ app/Http/Controllers/Company/CompanyController.php
@@ -2,10 +2,33 @@
 
 namespace App\Http\Controllers\Company;
 
+use App\Company;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class CompanyController extends Controller
 {
-    //
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function index()
+	{
+		$companies = Company::has('products')->get();
+
+		return response()->json(['data' => $companies], 200);
+	}
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function show($id)
+	{
+		$company = Company::has('products')->findOrFail($id);
+		return response()->json(['data' => $company], 200);
+	}
 }
--- app/Http/Controllers/Customer/CustomerController.php
+++ app/Http/Controllers/Customer/CustomerController.php
@@ -2,10 +2,33 @@
 
 namespace App\Http\Controllers\Customer;
 
+use App\Customer;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class CustomerController extends Controller
 {
-    //
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function index()
+	{
+		$customers = Customer::has('transactions')->get();
+
+		return response()->json(['data' => $customers], 200);
+	}
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function show($id)
+	{
+		 $customer = Customer::has('transactions')->findOrFail($id);
+		return response()->json(['data' => $customer], 200);
+	}
 }
