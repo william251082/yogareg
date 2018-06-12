f77eaedc0bd8feada33b91a26d632ed2731cb50d
William del Rosario <williamdelrosario@yahoo.com>
2018-06-12 11:43:09 +0200

    Single-reource controllers finished

--- app/Http/Controllers/Product/ProductController.php
+++ app/Http/Controllers/Product/ProductController.php
@@ -2,10 +2,27 @@
 
 namespace App\Http\Controllers\Product;
 
+use App\Product;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\ApiController;
 
-class ProductController extends Controller
+class ProductController extends ApiController
 {
-    //
+	public function index()
+	{
+		$products = Product::all();
+
+		return $this->showAll($products);
+	}
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  \App\Product  $product
+	 * @return \Illuminate\Http\Response
+	 */
+	public function show(Product $product)
+	{
+		return $this->showOne($product);
+	}
 }
