40713964f3739e2ac64c345f12e769bebfa7cf98
William del Rosario <williamdelrosario@yahoo.com>
2018-06-12 09:11:19 +0200

    UserController CRUD

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -20,4 +20,102 @@ class UserController extends Controller
 		return response()->json(['data' => $users], 200);
 		// return $users;
 	}
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function show($id)
+	{
+		$user = User::findOrFail($id);
+		return response()->json(['data' => $user], 200);
+	}
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return \Illuminate\Http\Response
+	 */
+	public function store(Request $request)
+	{
+		$rules = [
+			'name' => 'required',
+			'email' => 'required|email|unique:users',
+			'password' => 'required|min:6|confirmed',
+		];
+
+		$this->validate($request, $rules);
+
+		$data = $request->all();
+		$data['password'] = bcrypt($request->password);
+		$data['verified'] = User::UNVERIFIED_USER;
+		$data['verification_token'] = User::generateVerificationCode();
+		$data['admin'] = User::REGULAR_USER;
+
+		$user = User::create($data);
+
+		return response()->json(['data' => $user], 201);
+	}
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function update(Request $request, User $user)
+	{
+		$rules = [
+			'email' => 'email|unique:users',
+			'password' => 'min:6|confirmed',
+		];
+
+		if ($request->has('name')) {
+			$user->name = $request->name;
+		}
+
+		if ($request->has('email') && $user->email != $request->email) {
+			$user->verified = User::UNVERIFIED_USER;
+			$user->verification_token = User::generateVerificationCode();
+			$user->email = $request->email;
+		}
+
+		if ($request->has('password')) {
+			$user->password = bcrypt($request->password);
+		}
+
+		if ($request->has('admin')) {
+			if (!$user->isVerified()) {
+				return response()->json([ 'error' => 'Only verified users can modify the admin field', 'code' => 409], 409);
+			}
+
+			$user->admin = $request->admin;
+		}
+
+		if (!$user->isDirty()) {
+			return $this->response()->json(['error' => 'You need to specify a different value to update', 'code' => 422], 422);
+		}
+
+		$user->save();
+
+		return response()->json(['data' => $user], 200);
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function destroy($id)
+	{
+		$user = User::findOrFail($id);
+		$user->delete();
+		return response()->json(['data' => $user], 200);
+	}
+
 }
